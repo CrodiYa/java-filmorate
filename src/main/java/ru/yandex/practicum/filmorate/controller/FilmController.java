995c9d3b3f33f70bcc17ad04cadf1cdd8d6e0604
Crodi <Crodis@yandex.ru>
2025-10-29 23:32:54 +0300

    feat: Completely reworked Controller. Deleted everything that controller should not have. All operations go through FilmService. Add new endpoints.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,41 +1,39 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
 
-    private final HashMap<Long, Film> films = new HashMap<>();
-    private final AtomicLong idGenerator = new AtomicLong();
+    private final FilmService filmService;
 
     /**
-     * Private constructor to initialize ID generator with starting ID = 1.
+     * Constructor for dependency injection
      */
-    private FilmController() {
-        idGenerator.set(1);
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
     }
 
     /**
      * Handles GET method.
-     * <p>Retrieves all films from the storage.
+     * <p>Retrieves all filmsStorage from the storage.
      *
-     * @return Collection of all films.
+     * @return Collection of all filmsStorage.
      */
     @GetMapping
     public Collection<Film> getFilms() {
-        log.info("GET /films - returning {} films", films.size());
-        return films.values();
+        return filmService.getAllFilms();
     }
 
     /**
@@ -46,13 +44,7 @@ public class FilmController {
      */
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film) {
-
-        film.setId(idGenerator.getAndIncrement());
-        films.put(film.getId(), film);
-
-        log.info("POST /films - Film created: {}", film);
-
-        return film;
+        return filmService.addFilm(film);
     }
 
     /**
@@ -75,15 +67,46 @@ public class FilmController {
             throw new ValidationException("Id должен быть указан");
         }
 
-        if (films.containsKey(newFilm.getId())) {
-            Film oldFilm = films.get(newFilm.getId());
+        return filmService.updateFilm(newFilm);
+    }
 
-            films.put(newFilm.getId(), newFilm);
-            log.info("PUT /films - Film updated: {}", oldFilm);
+    /**
+     * Handles GET method.
+     * <p>Retrieves top {@code count} popular films based on likes.
+     *
+     * @param count must be provided by url params, not required default value is 10.
+     * @return collection of films.
+     */
+    @GetMapping("/popular")
+    public Collection<Film> getPopular(@RequestParam(required = false, defaultValue = "10") @Positive Long count) {
+        return filmService.getTopFilms(count);
+    }
 
-            return newFilm;
-        }
+    /**
+     * Handles PUT method.
+     * <p>Adds like to specific film by specific user. If like from this user is already set - nothing happens.
+     *
+     * @param filmId Film`s id where likes amount should be increased. Must be positive number.
+     * @param userId User`s id who sets like to the film. Must be positive number.
+     * @return Film where like was set.
+     */
+    @PutMapping("/{filmId}/like/{userId}")
+    public Film addLike(@PathVariable @Positive Long filmId,
+                        @PathVariable @Positive Long userId) {
+        return filmService.addLike(filmId, userId);
+    }
 
-        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+    /**
+     * Handles PUT method.
+     * <p>Deletes like from specific film by specific user. If like was never there - nothing happens.
+     *
+     * @param filmId Film`s id where likes amount should be decreased. Must be positive number.
+     * @param userId User`s id who deletes like to the film. Must be positive number.
+     * @return Film where like was deleted.
+     */
+    @DeleteMapping("/{filmId}/like/{userId}")
+    public Film deleteLike(@PathVariable @Positive Long filmId,
+                           @PathVariable @Positive Long userId) {
+        return filmService.removeLike(filmId, userId);
     }
 }

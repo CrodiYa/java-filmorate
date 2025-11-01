d8ee6490faacae0a4d02446918447e2329dae865
Crodi <Crodis@yandex.ru>
2025-11-01 06:04:57 +0300

    feat: reworked services, now business logic is only here.

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -1,11 +1,15 @@
 package ru.yandex.practicum.filmorate.service.film;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.LikeStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 /**
  * Main service for film operations and business logic.
@@ -16,21 +20,21 @@ import java.util.Collection;
  *
  * @see Film
  * @see FilmStorage
- * @see FilmLikeService
  */
 @Service
-@Slf4j
-public class FilmService {
+public class FilmService implements FilmServiceInterface {
 
     private final FilmStorage filmStorage;
-    private final FilmLikeService filmLikeService;
+    private final UserStorage userStorage;
+    private final LikeStorage likeStorage;
 
     /**
      * Constructor for dependency injection
      */
-    public FilmService(FilmStorage filmStorage, FilmLikeService filmLikeService) {
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage, LikeStorage likeStorage) {
         this.filmStorage = filmStorage;
-        this.filmLikeService = filmLikeService;
+        this.userStorage = userStorage;
+        this.likeStorage = likeStorage;
     }
 
     /**
@@ -38,8 +42,11 @@ public class FilmService {
      *
      * @param id Film`s id to get.
      * @return Film.
+     * @throws NotFoundException if film not found
      */
+    @Override
     public Film getFilm(Long id) {
+        throwIfNotFound(id);
         return filmStorage.get(id);
     }
 
@@ -48,6 +55,7 @@ public class FilmService {
      *
      * @return Collection of all filmsStorage.
      */
+    @Override
     public Collection<Film> getAllFilms() {
         return filmStorage.getAll();
     }
@@ -58,8 +66,11 @@ public class FilmService {
      * @param film Film to add.
      * @return created Film.
      */
+    @Override
     public Film addFilm(Film film) {
-        return filmStorage.add(film);
+        Film returnFilm = filmStorage.add(film);
+        likeStorage.initializeLikesSet(returnFilm.getId());
+        return returnFilm;
     }
 
     /**
@@ -67,8 +78,15 @@ public class FilmService {
      *
      * @param film Film to update.
      * @return updated Film.
+     * @throws NotFoundException if film not found
      */
+    @Override
     public Film updateFilm(Film film) {
+        if (film.getId() == null) {
+            throw new ValidationException("Id должен быть указан");
+        }
+        throwIfNotFound(film.getId());
+
         return filmStorage.update(film);
     }
 
@@ -76,9 +94,13 @@ public class FilmService {
      * Deletes film from the storage.
      *
      * @param id Film`s id to delete.
+     * @throws NotFoundException if film not found
      */
+    @Override
     public void deleteFilm(Long id) {
+        throwIfNotFound(id);
         filmStorage.remove(id);
+        likeStorage.clearLikesSet(id);
     }
 
     /**
@@ -87,9 +109,19 @@ public class FilmService {
      * @param filmId Film`s id to add like.
      * @param userId User`s id to set like.
      * @return Film where likes where added.
+     * @throws NotFoundException if film or user not found
      */
+    @Override
     public Film addLike(Long filmId, Long userId) {
-        return filmLikeService.addLike(filmId, userId);
+        throwIfNotFound(filmId);
+        if (!userStorage.contains(userId)) {
+            throw new NotFoundException("Пользователь с id = " + userId + " не найден");
+        }
+
+        Long newLikes = likeStorage.addLike(filmId, userId);
+        filmStorage.get(filmId).setLikes(newLikes);
+
+        return filmStorage.get(filmId);
     }
 
     /**
@@ -98,9 +130,19 @@ public class FilmService {
      * @param filmId Film`s id to delete like.
      * @param userId User`s id to delete like.
      * @return Film where likes where deleted.
+     * @throws NotFoundException if film or user not found
      */
+    @Override
     public Film removeLike(Long filmId, Long userId) {
-        return filmLikeService.removeLike(filmId, userId);
+        throwIfNotFound(filmId);
+        if (!userStorage.contains(userId)) {
+            throw new NotFoundException("Пользователь с id = " + userId + " не найден");
+        }
+
+        Long newLikes = likeStorage.deleteLike(filmId, userId);
+        filmStorage.get(filmId).setLikes(newLikes);
+
+        return filmStorage.get(filmId);
     }
 
     /**
@@ -109,8 +151,19 @@ public class FilmService {
      * @param count limit to collection.
      * @return collection of Films.
      */
+    @Override
     public Collection<Film> getTopFilms(Long count) {
-        return filmLikeService.getTopFilms(count);
+
+        return filmStorage.getAll().stream()
+                .sorted(Comparator.comparingLong(Film::getLikes).reversed())
+                .limit(count)
+                .toList();
     }
 
+    @Override
+    public void throwIfNotFound(Long id) {
+        if (!filmStorage.contains(id)) {
+            throw new NotFoundException("Фильм с id = " + id + " не найден");
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,10 +1,15 @@
 package ru.yandex.practicum.filmorate.service.user;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.FriendShipStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Main service for user operations and business logic.
@@ -15,34 +20,36 @@ import java.util.Collection;
  *
  * @see User
  * @see UserStorage
- * @see FriendShipService
  */
 @Service
-public class UserService {
+public class UserService implements UserServiceInterface {
 
     private final UserStorage userStorage;
-    private final FriendShipService friendShipService;
+    private final FriendShipStorage friendShipStorage;
 
     /**
      * Constructor for dependency injection
      */
-    public UserService(UserStorage userStorage, FriendShipService friendShipService) {
+    public UserService(UserStorage userStorage, FriendShipStorage friendShipStorage) {
         this.userStorage = userStorage;
-        this.friendShipService = friendShipService;
+        this.friendShipStorage = friendShipStorage;
     }
 
     /**
      * @param id ID of the user to be deleted. Must be positive and exist in the system.
      * @return User from storage.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException if user is not found.
+     * @throws NotFoundException if user is not found.
      */
+    @Override
     public User getUser(Long id) {
+        throwIfNotFound(id);
         return userStorage.get(id);
     }
 
     /**
      * @return Collection of all users in storage.
      */
+    @Override
     public Collection<User> getAllUsers() {
         return userStorage.getAll();
     }
@@ -50,17 +57,22 @@ public class UserService {
     /**
      * Add user to the storage.
      * <p>If user`s name is null, then login is set as a name.
+     * <p>Initializes set for friends.
      *
      * @param user User to create.
      * @return created User.
      */
+    @Override
     public User addUser(User user) {
 
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
 
-        return userStorage.add(user);
+        User returnUser = userStorage.add(user);
+        friendShipStorage.initializeFriendsSet(returnUser.getId());
+
+        return returnUser;
     }
 
     /**
@@ -68,18 +80,34 @@ public class UserService {
      *
      * @param user User to replace. User`s id must be in the system.
      * @return updated User.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException if user is not found
+     * @throws NotFoundException   if user is not found
+     * @throws ValidationException if ID is null
      */
+    @Override
     public User updateUser(User user) {
+        if (user.getId() == null) {
+            throw new ValidationException("Id должен быть указан");
+        }
+        throwIfNotFound(user.getId());
+
         return userStorage.update(user);
     }
 
     /**
      * Deletes user from the storage.
+     * Deletes user as from friends of all users.
      *
      * @param id ID of the user to be deleted. Must be positive and exist in the system.
+     * @throws NotFoundException if user not found
      */
+    @Override
     public void deleteUser(Long id) {
+        throwIfNotFound(id);
+
+        Set<Long> friendIds = friendShipStorage.getFriends(id);
+        friendIds.forEach(friendId -> friendShipStorage.getFriends(friendId).remove(id));
+        friendShipStorage.clearFriendsSet(id);
+
         userStorage.remove(id);
     }
 
@@ -88,11 +116,20 @@ public class UserService {
      *
      * @param senderId   ID of the user that sends friendship request. Must be positive and exist in the system.
      * @param receiverId ID of the user that gets friendship request. Must be positive and exist in the system.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException   if user is not found
-     * @throws ru.yandex.practicum.filmorate.exception.ValidationException if senderId equals receiverId
+     * @throws NotFoundException   if user is not found
+     * @throws ValidationException if senderId equals receiverId
      */
-    public Collection<User> addFriend(Long senderId, Long receiverId) {
-        return friendShipService.addFriend(senderId, receiverId);
+    @Override
+    public void addFriend(Long senderId, Long receiverId) {
+        if (Objects.equals(receiverId, senderId)) {
+            throw new ValidationException("Сам себя не добавишь - никто не добавит");
+        }
+
+        throwIfNotFound(senderId);
+        throwIfNotFound(receiverId);
+
+        friendShipStorage.addFriend(senderId, receiverId);
+        friendShipStorage.addFriend(receiverId, senderId);
     }
 
     /**
@@ -101,19 +138,29 @@ public class UserService {
      *
      * @param senderId   ID of the user from where friend should be deleted. Must be positive and exist in the system.
      * @param receiverId ID of the user that is being deleted. Must be positive and exist in the system.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException if user is not found
+     * @throws NotFoundException if user is not found
      */
+    @Override
     public void deleteFriend(Long senderId, Long receiverId) {
-        friendShipService.deleteFriend(senderId, receiverId);
+        throwIfNotFound(senderId);
+        throwIfNotFound(receiverId);
+
+        friendShipStorage.deleteFriend(senderId, receiverId);
+        friendShipStorage.deleteFriend(receiverId, senderId);
     }
 
     /**
      * @param id ID of the user to get friends. Must be positive and exist in the system.
      * @return Collection of all user`s friends.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException if user is not found
+     * @throws NotFoundException if user is not found
      */
+    @Override
     public Collection<User> getFriends(Long id) {
-        return friendShipService.getFriends(id);
+        throwIfNotFound(id);
+
+        return friendShipStorage.getFriends(id).stream()
+                .map(userStorage::get)
+                .toList();
     }
 
     /**
@@ -122,9 +169,26 @@ public class UserService {
      * @param id      ID of the first user. Must be positive and exist in the system.
      * @param otherId ID of the second user. Must be positive and exist in the system.
      * @return Collection of all common friends between users.
-     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException if user is not found
+     * @throws NotFoundException if user is not found
      */
+    @Override
     public Collection<User> getCommonFriends(Long id, Long otherId) {
-        return friendShipService.getCommonFriends(id, otherId);
+        throwIfNotFound(id);
+        throwIfNotFound(otherId);
+
+        Set<Long> friends = friendShipStorage.getFriends(id);
+        Set<Long> otherFriends = friendShipStorage.getFriends(otherId);
+
+        return friends.stream()
+                .filter(otherFriends::contains)
+                .map(userStorage::get)
+                .toList();
+    }
+
+    @Override
+    public void throwIfNotFound(Long id) {
+        if (!userStorage.contains(id)) {
+            throw new NotFoundException("Пользователь с id = " + id + " не найден");
+        }
     }
 }

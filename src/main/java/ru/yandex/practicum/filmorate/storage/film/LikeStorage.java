0fbd8a3446cf10e860d020c307f3e655cdb44508
Crodi <Crodis@yandex.ru>
2025-11-20 17:07:27 +0300

    feat: add Film and Like db storages

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/LikeStorage.java
@@ -2,9 +2,8 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 public interface LikeStorage {
 
-    void initializeLikesSet(Long id);
-
-    void clearLikesSet(Long id);
+    default void clearLikes(Long id) {
+    }
 
     Long addLike(Long filmId, Long userId);
 
